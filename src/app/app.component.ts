bc6d77942d539494949872e13ff0dc7896d3e5d9
meireliezer <meireliezer@hotmail.com>
2020-02-26 10:21:41 +0200

    install bootstrap & fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCoffee, faTable, faUser } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wdapt-bs';
+  faCoffee = faCoffee;
+  faTable = faTable;
+  faUser = faUser;
+
+  constructor(){
+    console.log('faTable', faTable);
+  }
+  
 }
