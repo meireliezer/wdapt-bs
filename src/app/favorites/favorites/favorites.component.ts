537d3e42a46153bb43127ac4e0063aa2061ed37d
meireliezer <meireliezer@hotmail.com>
2020-03-01 13:47:04 +0200

    filter

--- src/app/favorites/favorites/favorites.component.ts
+++ src/app/favorites/favorites/favorites.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { faPlus, faTable, faList, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { ActivatedRoute } from '@angular/router';
 import { NgbModal, NgbModule, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { AddDialogComponent } from '../dialogs/add-dialog/add-dialog.component';
 import { FavoritesService } from 'src/app/common/favorites/favorites.service';
+import { fromEvent,  } from 'rxjs';
+import { map, distinctUntilChanged, debounce, debounceTime, tap } from 'rxjs/operators';
 
 
 
@@ -12,7 +14,7 @@ import { FavoritesService } from 'src/app/common/favorites/favorites.service';
   templateUrl: './favorites.component.html',
   styleUrls: ['./favorites.component.css']
 })
-export class FavoritesComponent{
+export class FavoritesComponent implements AfterViewInit {
 
   public faPlus = faPlus;
   public faList = faList;
@@ -23,6 +25,9 @@ export class FavoritesComponent{
     return this._isGridView;
   }
   
+
+  @ViewChild('search', {static:true})
+  search: ElementRef;
   
   private _isGridView;
   private ngbModalRef: NgbModalRef;
@@ -36,6 +41,21 @@ export class FavoritesComponent{
     });
 
   }
+
+  ngAfterViewInit(): void {
+    console.log('search', this.search);
+    
+    let filter$ = fromEvent(this.search.nativeElement, 'keyup').pipe(
+      map( (event:any) => event.target.value.trim()),
+      debounceTime(400),
+      distinctUntilChanged(),
+      tap(val => console.log(val))
+    );
+
+    filter$.subscribe(val => console.log( 'search: ', val) ); // and now filter the data
+  }
+
+
 
   public add(){
     this.ngbModalRef = this.modalService.open(AddDialogComponent);
